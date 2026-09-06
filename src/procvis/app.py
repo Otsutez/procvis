36b02affb4b0a012d4b06f9aeb9f535ba970ad85
Otsute <rahatpracharktam@gmail.com>
2026-09-06 23:39:30 +1000

    Pause timer when entering process view

--- src/procvis/app.py
+++ src/procvis/app.py
@@ -4,6 +4,7 @@ from textual import work
 from textual.app import App, ComposeResult
 from textual.containers import CenterMiddle
 from textual.reactive import reactive
+from textual.timer import Timer
 from textual.widgets import ContentSwitcher, Footer, Header
 from textual.worker import Worker, get_current_worker
 
@@ -18,37 +19,36 @@ class ProcessVisualiserApp(App):
     CSS_PATH = "app.tcss"
     TITLE = "Ram Visualiser"
     stats: reactive[dict[int, Stat]] = reactive({})
-    pids: reactive[list[int]] = reactive([])
+    timer: Timer | None = None
 
     def on_mount(self) -> None:
         """Start worker to fetch pids"""
-        self.update_pids()
-        self.set_interval(5, self.update_pids)
+        self.update_stats()
+        self.timer = self.set_interval(5, self.update_stats)
 
     @work(exclusive=True, thread=True)
-    def update_pids(self) -> None:
+    def update_stats(self) -> None:
         worker = get_current_worker()
         pids = MemoryReader.get_pids()
-        if not worker.is_cancelled:
-            self.call_from_thread(self.set_pids, pids)
 
-    def set_pids(self, pids: list[int]) -> None:
-        self.pids = pids
-
-    def on_worker_state_changed(self, event: Worker.StateChanged) -> None:
-        self.log(event)
-
-    def compute_stats(self) -> dict[int, Stat]:
-        """Retrieve stats for all pids when pids updated"""
         stats: dict[int, Stat] = {}
-        for pid in self.pids:
+        for pid in pids:
             stat = MemoryReader.read_stat(pid)
             if stat:
                 stats[pid] = stat
             else:
                 self.log(f"Error: failed to read stat for process: {pid}")
 
-        return stats
+        if not worker.is_cancelled:
+            self.call_from_thread(self.set_stats, stats)
+
+    def set_stats(self, stats: dict[int, Stat]) -> None:
+        self.stats = stats
+        selector = self.query_one(ProcessSelector)
+        selector.stats = list(stats.values())
+
+    def on_worker_state_changed(self, event: Worker.StateChanged) -> None:
+        self.log(event)
 
     def watch_stats(self, stats: dict[int, Stat]) -> None:
         selector = self.query_one(ProcessSelector)
@@ -64,10 +64,14 @@ class ProcessVisualiserApp(App):
         yield Footer()
 
     def on_process_selector_selected(self, event: ProcessSelector.Selected):
-        if event.pid is not None:
-            self.query_one(ProcessView).pid = event.pid
+        if event.pid is not None and event.pid in self.stats:
+            if self.timer:
+                self.timer.pause()
+            self.query_one(ProcessView).stat = self.stats[event.pid]
             self.query_one(ContentSwitcher).current = "process-view"
             self.log(f"{event.pid} selected")
+        else:
+            self.log(f"Failed to enter process view for {event.pid}, no data")
 
 
 if __name__ == "__main__":
